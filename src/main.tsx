c23f6077dd799ebf2ecde771efc0991432f94f57
Shubham Mate <111609339+ProSrm@users.noreply.github.com>
2025-01-02 17:44:35 +0530

    added connection with .net backend (#6)
    Co-authored-by: Shubham Mate <shubham.r.mate@softmass.co.in>

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './css/index.css'
 import Dashboard from './componant/Dashboard/Dashboard.tsx'
 import ShipmentStatistics from './componant/Dashboard/Chart.tsx'
 import App from './componant/App.tsx'
+import ProductList from './componant/Products/ProductList.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* <ProductList /> */}
     {/* <Dashboard /> */}
     {/* <ShipmentStatistics/> */}
   </StrictMode>,
